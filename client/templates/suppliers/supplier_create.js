263256ab1146b42ba5b02d83b6531fe148c4e601
Brian J Lennon <mrbrianlennon@gmail.com>
2015-10-01 12:03:49 +0100

    Commit 7-6 Use a method to create supplier

--- client/templates/suppliers/supplier_create.js
+++ client/templates/suppliers/supplier_create.js
@@ -8,6 +8,13 @@ Template.supplierCreate.events({
       accountNo: $(e.target).find('[name=accountNo]').val()
     };
 
+    Meteor.call('supplierInsert', supplier, function(error, result) {
+      // display the error to the user and abort
+      if (error)
+        return alert(error.reason);
+      Router.go('supplierPage', {_id: result._id});  
+    });
+
     supplier._id = Suppliers.insert(supplier);
     Router.go('supplierPage', supplier);
   }
