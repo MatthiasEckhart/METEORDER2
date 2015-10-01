dd5f2be119d45b241400413a3e2d58405172d518
Brian J Lennon <mrbrianlennon@gmail.com>
2015-10-01 19:49:08 +0100

    Commit 9-4 Validate supplier contents on submission.

--- client/templates/suppliers/supplier_create.js
+++ client/templates/suppliers/supplier_create.js
@@ -1,3 +1,15 @@
+Template.supplierCreate.onCreated(function() {
+  Session.set('supplierCreateErrors', {});
+});
+Template.supplierCreate.helpers({
+  errorMessage: function(field) {
+    return Session.get('supplierCreateErrors')[field];
+  },
+  errorClass: function (field) {
+    return !!Session.get('supplierCreateErrors')[field] ? 'has-error' : '';
+  }
+});
+
 Template.supplierCreate.events({
   'submit form': function(e) {
     e.preventDefault();
@@ -7,6 +19,11 @@ Template.supplierCreate.events({
       supplierEmail: $(e.target).find('[name=supplierEmail]').val(),
       accountNo: $(e.target).find('[name=accountNo]').val()
     };
+
+    var errors = validateSupplier(supplier);
+    if (errors.supplierName || errors.supplierEmail || errors.accountNo)
+      return Session.set('supplierCreateErrors', errors);
+
 
     Meteor.call('supplierInsert', supplier, function(error, result) {
       // display the error to the user and abort
