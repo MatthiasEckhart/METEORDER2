c2e65ee1fbab61ef5bcf947dd7b36fa2f5871853
Brian J Lennon <mrbrianlennon@gmail.com>
2015-10-02 16:02:34 +0100

    Headers, "Category", errors, link
    Added some headings to the templates
    Added “Category” when adding a new product
    Improved error handling when creating product
    You can now click anywhere on supplierName <li> and bring up products

--- client/templates/products/product_create.js
+++ client/templates/products/product_create.js
@@ -17,11 +17,13 @@ Template.productCreate.events({
 
     var $sku = $(e.target).find('[name=sku]');
     var $description = $(e.target).find('[name=description]');
+    var $category = $(e.target).find('[name=category]');
     var $unit = $(e.target).find('[name=unit]');
     var $price = $(e.target).find('[name=price]');
     var product = {
       sku: $sku.val(),
       description: $description.val(),
+      category: $category.val(),
       unit: $unit.val(),
       price: $price.val(),
       supplierId: template.data._id
@@ -32,12 +34,33 @@ Template.productCreate.events({
       errors.sku = "Please enter SKU";
       return Session.set('productCreateErrors', errors);
     }
+    if (! product.description) {
+      errors.description = "Please enter description";
+      return Session.set('productCreateErrors', errors);
+    }
+     if (! product.category) {
+      errors.category = "Please enter category";
+      return Session.set('productCreateErrors', errors);
+    }
+    if (! product.unit) {
+      errors.unit = "Please enter unit";
+      return Session.set('productCreateErrors', errors);
+    }
+    if (! product.price) {
+      errors.price = "Please enter price";
+      return Session.set('productCreateErrors', errors);
+    }
 
     Meteor.call('productCreate', product, function(error, productId) {
       if (error){
         throwError(error.reason);
       } else {
         $sku.val('');
+        $description.val('');
+        $category.val('');
+        $unit.val('');
+        $price.val('');
+
       }
     });
   }
