c65a8a404090c5b2032152471f749eb1e72507fc
Brian J Lennon <mrbrianlennon@gmail.com>
2015-10-06 21:38:36 +0100

    Nothing of signficance

--- client/templates/includes/register.js
+++ client/templates/includes/register.js
@@ -1,9 +1,11 @@
 Template.register.events({
     'submit form': function(event) {
         event.preventDefault();
+        // var username = $('[name=username]').val();
         var email = $('[name=email]').val();
         var password = $('[name=password]').val();
         Accounts.createUser({
+            username: username,
             email: email,
             password: password
         }, function(error) {
