14ee0bdd67bd81174dd735dee8e8aadb36b0b10d
Brian J Lennon <mrbrianlennon@gmail.com>
2015-11-10 20:14:55 +0000

    Orders being built correctly

--- client/templates/products/product_item.js
+++ client/templates/products/product_item.js
@@ -6,11 +6,13 @@ Template.productItem.helpers({
 
 Template.productItem.events({
     'click .addOrder':function(evt,tmpl){
+      var userId = Meteor.userId();
+      var selectedSupplier = Session.get('selectedSupplier');
       var qty1 = tmpl.find('.prodqty').value;
       var qty = parseInt(qty1);
         var product = this._id;
         var sessid = Meteor.default_connection._lastSessionId; //stops others  adding to your cart etc
-         console.log('this is the quantity:', typeof qty, product, sessid);//stops others  ad
-        Meteor.call('addToOrder',qty, product, sessid);
+         console.log('this is the quantity:', typeof qty, product, sessid);
+        Meteor.call('addToOrder',userId, selectedSupplier, qty, product, sessid);
     }
 });
--- server/server.js
+++ server/server.js
@@ -2,51 +2,109 @@ Meteor.methods({
 
     // BL added 20151109 
     createOrder: function(userId, supplierId) {
-        check(supplierId, String);
-        check(userId, String);
-        if (!openOrderExists(supplierId)) {
-            Orders.insert({
-                user: userId,
-                supplier: supplierId,
-                orderItems: [],
-                status: 0
-            });
-            console.log('order created maLord');
-        } else {
-            console.log('order already exists mLady');
-        }
-    },
+            check(supplierId, String);
+            check(userId, String);
+            if (!openOrderExists(supplierId)) {
+                Orders.insert({
+                    user: userId,
+                    supplier: supplierId,
+                    orderItems: [],
+                    status: 0
+                });
+                console.log('order created maLord');
+            } else {
+                console.log('order already exists mLady');
+            }
+        },
+        addToOrder: function(userId, selectedSupplier, qty, product, session) {
+            check(userId, String);
+            check(selectedSupplier, String);
+            check(qty, Number);
+            check(product, String);
+            check(session, String);
+            if (qty > 0) {
+                if (!alreadyAdded(userId, selectedSupplier, product)) {
+                    Orders.update({
+                        user: userId,
+                        supplier: selectedSupplier,
+                        status: 0
+                    }, {
+                        $push: {
+                            orderItems: {
+                                $each: [{
+                                    product: product,
+                                    qty: qty
+                                }]
+                            }
+                        }
+                    });
+
+                    console.log('New product '+product+' added to order');
+                } else {
+                    Orders.update({
+                        user: userId,
+                        supplier: selectedSupplier,
+                        status: 0,
+                        'orderItems.product': product
+                    }, {
+                        $set: {
+                            'orderItems.$.qty': qty
+                        }
+                    });
+                   console.log('Product '+product+' updated');
+                }
+            } else {
+                console.log('Quantity is Zero');
+            }
+
+        },
 
     // end
 
-    addToOrder: function(qty, product, session) {
-        console.log('Im here!');
-        check(qty, Number);
-        check(product, String);
-        check(session, String);
-        if (qty > 0) {
-            if (!alreadyAdded(product)) {
-                OrderItems.insert({
-                    qty: qty,
-                    product: product,
-                    sessid: session
-                });
-                console.log('reaching this fn', qty, product, session);
-            } else {
-                OrderItems.update({
-                    product: product
-                }, {
-                    qty: qty,
-                    product: product,
-                    sessid: session
-                })
-                console.log('Successfully updated');
-            }
-        } else {
-            console.log('Quantity is Zero');
-        }
+    /* addToOrder: function(userId, qty, product, session) {
+     console.log(typeof userId);
+     check(userId, String);
+    // check(supplierId, String);
+     check(qty, Number);
+     check(product, String);
+     check(session, String);
+     if (qty > 0) {
+         if (!alreadyAdded(product)) {
+             Orders.update(
+             {
+                 user: userId,
+                 //supplier: supplierId,
+                 status: 0
+             }, 
+             {
+                 $push: {
+                     orderItems: {$each: [{product:product, qty:qty}]}
+                 }
+             });
 
-    },
+             console.log('reaching this fn', qty, product, session);
+         } 
+
+         else {
+             Orders.findAndModify({
+                query: {user: userId,
+                 //supplier: supplierId,
+                 status: 0 },
+                 update: 
+                 product: product
+                }
+             }, {
+                 qty: qty,
+                 product: product,
+                 sessid: session
+             })
+             console.log('Successfully updated');
+         }
+     } else {
+         console.log('Quantity is Zero');
+     }
+
+ },*/
     removeOrderItem: function(id) {
         check(id, String);
         OrderItems.remove({
@@ -59,12 +117,26 @@ Meteor.methods({
 
 });
 
+/* this works for orderitems  
+
 function alreadyAdded(product) {
-    if (!OrderItems.findOne({
+    if (!orderItems.findOne({
             product: product
         })) {
         return false;
     } else {
+        return true;
+    }
+}
+
+*/
+
+function alreadyAdded(userId, selectedSupplier, product) {
+    if (!Orders.findOne({ user: userId, supplier:selectedSupplier, orderItems:{ $elemMatch: {product:product}}})) {
+        console.log('Prod not already added to order');
+        return false;
+    } else {
+         console.log('Prod IS already added to order');
         return true;
     }
 }
