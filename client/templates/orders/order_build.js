c819f981a3f8efdd797a8924be8a05ea789f7cce
Brian J Lennon <mrbrianlennon@gmail.com>
2015-11-10 23:31:30 +0000

    Returning to a working state

--- client/templates/orders/order_build.js
+++ client/templates/orders/order_build.js
@@ -1,3 +1,47 @@
+/*new version BJL 2015 11 10
+
+Template.order.helpers({
+    orderItems: function () {
+      var userId = Meteor.userId();
+      var selectedSupplier = Session.get('selectedSupplier');
+        return Orders.find({user:userId, supplier: selectedSupplier, status:0},{orderItems:1}).map((orderItem) => {
+            let product = Products.findOne({
+                _id: orderItem.orderItems.product
+            });
+            if (product) {
+                orderItem.productname = product.description;
+                orderItem.price = calcPrice(product, orderItem);
+                return orderItem;
+            }
+        });
+    },
+    orderCart: function () {
+        let orderCart = {subtotal: 0};
+        OrderItems.find().forEach((orderItem) => {
+            let product = Products.findOne({
+                _id: orderItem.product
+            });
+            if (product) orderCart.subtotal += calcPrice(product, orderItem);
+        });
+        
+        orderCart.tax = orderCart.subtotal * .23;
+        orderCart.total = orderCart.subtotal + orderCart.tax;
+        return orderCart;
+    }
+});
+
+function calcPrice(product, orderItem) {
+    return (Number(product.price) * orderItem.qty);
+}
+
+Template.order.events({
+    'click .removeci': function(evt, tmpl) {
+        Meteor.call('removeOrderItem', this._id);
+    }
+});
+
+*/ //original version
+
 Template.order.helpers({
     orderItems: function () {
         return OrderItems.find().map((orderItem) => {
@@ -35,3 +79,5 @@ Template.order.events({
         Meteor.call('removeOrderItem', this._id);
     }
 });
+
+//*/
