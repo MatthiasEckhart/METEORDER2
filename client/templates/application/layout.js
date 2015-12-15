178777aaebea390925231d9214e5b8c7d5dd1b6b
Brian J Lennon <mrbrianlennon@gmail.com>
2015-12-15 09:53:47 +0000

    Added MUP and filled out correct details
    App deployed to properorder.co.uk except the product template won’t load

--- client/templates/application/layout.js
+++ client/templates/application/layout.js
@@ -1,13 +1,8 @@
 Template.layout.events({
-
     'click .supplier-sidebar-show': function() {
-    
         console.log('button pressed');
        $('.ui.sidebar').sidebar('setting', { dimPage: false });
         $('.ui.sidebar').sidebar('toggle');
 	}
-
-	
-
 });
 
